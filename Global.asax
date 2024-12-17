ecba7255a8a9d42ce84dfd674cbed879b19f0d45
Mahdi Zangeneh <122305994+matty900@users.noreply.github.com>
2024-12-17 12:59:02 -0500

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MZ2247A6.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MZ2247A6.Startup))]
+
+namespace MZ2247A6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
